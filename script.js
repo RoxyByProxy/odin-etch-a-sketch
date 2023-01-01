44f11899214f199e75957f8cdb7d950195e7dde1
RoxyByProxy <116697437+RoxyByProxy@users.noreply.github.com>
2022-12-31 22:52:55 -0500

    Pseudocode for base completion

---
+++ script.js
@@ -0,0 +1,6 @@
+//initialize button and container div
+//create function to draw squares
+//draw squares and append to container div
+//create function to darken squares by adding a class
+//set up listeners on squares
+//proceed to button and prompt
